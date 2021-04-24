b20beb1ec050468e9389b63c6f1002a2ebf66f9d
Arihant Jain <jainarihant231@gmail.com>
2021-04-24 22:35:24 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import This from './This';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <This />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
